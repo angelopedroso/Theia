397853cd3b5d7a39cf804dfc4ba64e5615b36a60
angelopedroso <solonasusonly0@gmail.com>
2023-07-18 20:23:01 -0300

    feat: added Card's paragraph

--- src/components/Summary/components/Card.tsx
+++ src/components/Summary/components/Card.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion'
-import { IconProps } from 'phosphor-react'
+import { IconProps } from '@phosphor-icons/react'
 
-import React from 'react'
+import React, { useRef } from 'react'
 
 export interface CardProps {
   Icon: React.ForwardRefExoticComponent<
@@ -13,20 +13,32 @@ export interface CardProps {
 }
 
 export function Card({ iconColor = 'text-indigo-600', ...props }: CardProps) {
+  const paragraphRef = useRef<HTMLParagraphElement>({} as HTMLParagraphElement)
+
+  const paragraphHeight = paragraphRef.current?.offsetHeight
+
   return (
-    <motion.div className="flex min-h-[8rem] min-w-[15rem] flex-col gap-3 rounded-md bg-slate-850 p-6 shadow-md">
+    <motion.div className="flex max-h-32 min-h-card min-w-card max-w-2sm flex-col gap-3 rounded-md bg-slate-850 p-6 shadow-md">
       <header className="flex items-start justify-between self-stretch">
-        <h3 className="font-main text-base font-medium text-gray-100">
-          {props.title}
-        </h3>
+        <h2 className="text-base font-medium text-gray-100">{props.title}</h2>
         <div className="p-1">
           <props.Icon className={`h-5 w-5 ${iconColor}`} />
         </div>
       </header>
-      <main>
+      <main
+        className={`flex ${
+          paragraphHeight > 16 ? 'items-center' : 'items-baseline'
+        } gap-2`}
+      >
         <strong className="font-menu text-4xl font-bold text-white">
           {props.data}
         </strong>
+        <p
+          ref={paragraphRef}
+          className="break-words font-menu text-xs text-gray-400"
+        >
+          Commands used
+        </p>
       </main>
     </motion.div>
   )
