df545fc7b9deae0654e6fe44f712b7112ca78135
angelopedroso <solonasusonly0@gmail.com>
2023-07-19 15:37:05 -0300

    feat: updated columnButton to be reusable

--- src/components/ui/columnSortingButton.tsx
+++ src/components/ui/columnSortingButton.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import { CaretUpDown } from '@phosphor-icons/react'
 import { Column } from '@tanstack/react-table'
-import { GroupTableProps } from '../DataTable/components'
 import { Button } from './button'
 
-export interface columnButtonSortingProps {
+export interface columnButtonSortingProps<T> {
   title: string
-  column: Column<GroupTableProps, unknown>
+  column: Column<T, unknown>
+  isGroup?: boolean
 }
 
-export function ColumnSortingButton({
+export function ColumnSortingButton<T>({
   title,
   column,
-}: columnButtonSortingProps) {
+  isGroup,
+}: columnButtonSortingProps<T>) {
   return (
     <Button
       variant={'ghost'}
       onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-      className={
-        'whitespace-nowrap border border-transparent text-sm font-medium leading-normal text-gray-100 hover:border-slate-800 hover:bg-slate-750 hover:text-gray-100'
-      }
+      className={`whitespace-nowrap border border-transparent leading-normal hover:border-slate-800 hover:bg-slate-750 ${
+        isGroup
+          ? 'text-sm font-medium  text-gray-100 hover:text-gray-100'
+          : 'text-xs font-semibold text-gray-200 hover:text-gray-200'
+      }`}
     >
       {title}
       <CaretUpDown className="ml-2 h-4 w-4" weight="fill" />
