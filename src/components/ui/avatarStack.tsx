204ff21847b53f189a21b3b1957e72a9e584f475
angelopedroso <solonasusonly0@gmail.com>
2023-07-19 01:07:17 -0300

    fix: fixed font size on the avatar stack

--- src/components/ui/avatarStack.tsx
+++ src/components/ui/avatarStack.tsx
@@ -13,7 +13,7 @@ export function AvatarStack(props: AvatarStackProps) {
 
   return (
     <div className="flex">
-      <div className="flex first:-ml-0">
+      <div className="ml-3 flex">
         {participants.slice(0, 3).map((participant, index) => {
           return (
             <Avatar className="-ml-3 h-9 w-9 border border-white" key={index}>
@@ -26,7 +26,13 @@ export function AvatarStack(props: AvatarStackProps) {
         })}
         {hasMoreParticipants && (
           <Avatar className="-ml-3 h-9 w-9 border border-white">
-            <AvatarFallback className="bg-indigo-600 text-xs font-medium text-white">
+            <AvatarFallback
+              className={`bg-indigo-600 ${
+                participants.length - 3 > 999
+                  ? 'text-[0.625rem]'
+                  : 'text-[0.6875rem]'
+              } font-medium text-white`}
+            >
               {`+${participants.length - 3}`}
             </AvatarFallback>
           </Avatar>
