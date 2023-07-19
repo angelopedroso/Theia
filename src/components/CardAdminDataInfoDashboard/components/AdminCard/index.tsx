a37f5781b20f48ee2c3a4e4335a1292edc6e4d33
angelopedroso <solonasusonly0@gmail.com>
2023-07-19 15:38:05 -0300

    feat: created admin card table

--- src/components/CardAdminDataInfoDashboard/components/AdminCard/index.tsx
+++ src/components/CardAdminDataInfoDashboard/components/AdminCard/index.tsx
@@ -1,7 +1,22 @@
+import { DataTable } from '@/components/DataTable/components'
 import React from 'react'
+import { column } from './columns'
+import { getDBData } from '@/api/axios/getDBData'
 
-export interface AdminCardProps {}
+export async function AdminCard() {
+  const data = await getDBData({
+    uri: 'users/admin',
+    revalidateTimeInSeconds: 60 * 10,
+  })
 
-export function AdminCard(props: AdminCardProps) {
-  return <div />
+  return (
+    <div>
+      <header>
+        <h2 className="p-4 text-base leading-normal text-gray-100">Admins</h2>
+      </header>
+      <main>
+        <DataTable columns={column} data={data} />
+      </main>
+    </div>
+  )
 }
