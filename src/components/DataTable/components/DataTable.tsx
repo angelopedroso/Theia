4ea4d10854acd661c226f6a8c1f96a2ac9a12335
angelopedroso <solonasusonly0@gmail.com>
2023-07-21 23:01:58 -0300

    feat: added optional filter input on table

--- src/components/DataTable/components/DataTable.tsx
+++ src/components/DataTable/components/DataTable.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { NoResults } from '@/components/noResults'
+import { FilterBy } from '@/components/ui/filterBy'
+
 import {
   Table,
   TableBody,
@@ -9,23 +12,28 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { DataTablePagination } from '@/components/ui/tableButton'
+
 import {
   ColumnDef,
+  ColumnFiltersState,
   SortingState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
+
 import React, { useState } from 'react'
 
 export interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
   tableWidth?: string
-  tableHeight?: 'min-h-xxl' | 'min-h-xl'
+  tableHeight?: string
   pageSize?: number
+  filterTable?: boolean
 }
 
 export function DataTable<TData, TValue>({
@@ -34,8 +42,11 @@ export function DataTable<TData, TValue>({
   tableWidth = '',
   tableHeight = 'min-h-xxl',
   pageSize = 5,
+  filterTable = false,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [filterBy, setFilterBy] = useState<string>('chat_name')
 
   const table = useReactTable({
     columns,
@@ -44,61 +55,73 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     initialState: { pagination: { pageSize } },
     state: {
       sorting,
+      columnFilters,
     },
   })
 
   return (
-    <div className={`grid ${tableHeight} grid-rows-table`}>
-      <Table className={`relative ${tableWidth}`}>
-        <TableHeader className="border-none bg-slate-825">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow
-              key={headerGroup.id}
-              className="border-b border-t border-slate-855 hover:bg-slate-825"
-            >
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead className="" key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows.length ? (
-            table.getRowModel().rows.map((row) => (
+    <div className="space-y-8">
+      {filterTable && (
+        <FilterBy filter={filterBy} setFilterBy={setFilterBy} table={table} />
+      )}
+
+      <div className={`grid ${tableHeight} grid-rows-table`}>
+        <Table className={`relative ${tableWidth}`}>
+          <TableHeader className="rounded-lg border-none bg-slate-825">
+            {table.getHeaderGroups().map((headerGroup) => (
               <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-                className="border-none hover:bg-slate-700"
+                key={headerGroup.id}
+                className="border-b border-t border-slate-855 hover:bg-slate-825"
               >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id} className="px-4 py-2">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead className="" key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  )
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow className="border-none hover:bg-slate-700">
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <DataTablePagination table={table} />
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                  className="border-none hover:bg-slate-700"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="px-4 py-2">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow className="border-none hover:bg-transparent">
+                <TableCell colSpan={columns.length} className="h-[33.8rem]">
+                  <NoResults />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <DataTablePagination table={table} />
+      </div>
     </div>
   )
 }
