2e636c7a7b7156c7795712a31e3f1a3aac7a6d6b
angelopedroso <solonasusonly0@gmail.com>
2023-07-21 22:52:50 -0300

    fix: removed unnecessary padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
 
   return (
     <>
-      <header className="h-44 w-full bg-indigo-600 p-8 px-8 pb-28">
+      <header className="h-44 w-full bg-indigo-600 p-8 pb-28">
         <h2 className="text-sm font-bold leading-normal text-zinc-50 xxs:text-lg sm:text-2xl">
           Summary
         </h2>
