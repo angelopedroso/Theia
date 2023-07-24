497e32ae2e1f203687f7bc3a05405871ebc4d09b
angelopedroso <solonasusonly0@gmail.com>
2023-07-24 14:10:31 -0300

    feat: added new date format

--- src/utils/formatValues.ts
+++ src/utils/formatValues.ts
@@ -3,15 +3,29 @@ export function formatNumberToThousandSeparator(value: number) {
 }
 
 export function formatDate(date: string) {
+  const currentDate = new Date(date)
+  const today = new Date()
+  const yesterday = new Date(today)
+  yesterday.setDate(today.getDate() - 1)
+
+  const isToday = currentDate.toDateString() === today.toDateString()
+  const isYesterday = currentDate.toDateString() === yesterday.toDateString()
+
+  if (isToday || isYesterday) {
+    const hours = String(currentDate.getHours()).padStart(2, '0')
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0')
+
+    return `${isToday ? 'Today' : 'Yesterday'}, ${hours}:${minutes}`
+  }
+
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
-    month: 'numeric',
+    month: 'short',
     day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
     hour12: false,
   }
 
-  return new Intl.DateTimeFormat('default', options).format(new Date(date))
+  return new Intl.DateTimeFormat('en-US', options).format(currentDate)
 }
