8ad1ce248ef09db6613bef32549f091c4c7c8ebd
angelopedroso <solonasusonly0@gmail.com>
2023-07-24 21:01:13 -0300

    feat: created group typing

---
+++ src/@types/group.d.ts
@@ -0,0 +1,30 @@
+interface Participant {
+  id: string
+  p_id: string
+  image_url: string | null
+  name: string
+}
+
+type GroupsProps = {
+  id: string
+  group_info: {
+    groupId: string
+    name: string
+    inviteCode: string
+    image_url?: string
+  }
+  g_id: string
+  bem_vindo: boolean
+  anti_link: boolean
+  anti_porn: boolean
+  one_group: boolean
+  auto_sticker: boolean
+  auto_invite_link: boolean
+  anti_trava_id: string
+  anti_trava: {
+    status: boolean
+    max_characters: number
+  }
+  black_list: Participant[]
+  participants: Participant[]
+}
