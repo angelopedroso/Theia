4c8b4bd2690795aa0f6fe8f47e9dbb261579ef0f
angelopedroso <solonasusonly0@gmail.com>
2023-07-24 21:03:25 -0300

    feat: added group page's content

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,12 +1,27 @@
+import { getDBData } from '@/api/getDBData'
+import { CardGroupMotion } from '@/components/GroupCard/cardGroup'
+
 import { Header } from '@/components/ui/header'
 import React from 'react'
 
-export interface pageProps {}
+export default async function Groups() {
+  const data: GroupsProps[] = await getDBData({
+    uri: 'groups',
+    revalidateTimeInSeconds: 60 * 5,
+  })
 
-export default function Groups(props: pageProps) {
   return (
-    <div className="flex h-full flex-col space-y-14 p-8">
+    <div className="flex h-full flex-col p-8">
       <Header title="Groups" subtitle="List of all active groups" />
+      <main className="flex h-full items-start px-16">
+        <div className="grid w-full grid-cols-2 place-items-center gap-8 lg:grid-cols-3">
+          {data.map((group) => {
+            return (
+              <CardGroupMotion layoutId={group.id} key={group.id} {...group} />
+            )
+          })}
+        </div>
+      </main>
     </div>
   )
 }
