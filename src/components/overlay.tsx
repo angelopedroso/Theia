3e783dfe3b405f4e511407740605e475ed5a8332
angelopedroso <solonasusonly0@gmail.com>
2023-07-24 23:42:54 -0300

    feat: created overlay component

---
+++ src/components/overlay.tsx
@@ -0,0 +1,25 @@
+import { motion } from 'framer-motion'
+import React, { ReactNode } from 'react'
+
+export interface OverlayProps {
+  children: ReactNode
+  close: () => void
+}
+
+export function Overlay(props: OverlayProps) {
+  return (
+    <motion.div
+      className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black/60"
+      onClick={props.close}
+      variants={{
+        open: { backgroundColor: 'rgba(0,0,0,0.6)' },
+        closed: { backgroundColor: 'rgba(0,0,0,0)' },
+      }}
+      initial="closed"
+      exit={'closed'}
+      animate={'open'}
+    >
+      {props.children}
+    </motion.div>
+  )
+}
