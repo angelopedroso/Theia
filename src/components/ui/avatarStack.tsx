36572e805439ac64fdb46644767c07d50843a246
angelopedroso <solonasusonly0@gmail.com>
2023-07-24 21:00:49 -0300

    fix: updated avatar stack border

--- src/components/ui/avatarStack.tsx
+++ src/components/ui/avatarStack.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react'
-import { UserDefaultIcon } from '../userIcon'
-import { GroupInfo, Participant } from '../DataTable/components'
 import { Avatar, AvatarFallback, AvatarImage } from './avatar'
+import { GroupInfo, Participant } from '../GroupDashboardTable/components'
 
 export interface AvatarStackProps {
   users: Participant[] | GroupInfo[]
@@ -12,33 +11,29 @@ export function AvatarStack({ users, children }: AvatarStackProps) {
   const hasMoreParticipants = users?.length > 3
 
   return (
-    <div className="flex">
-      <div className="ml-3 flex">
-        {users?.slice(0, 3).map((participant, index) => {
-          return (
-            <Avatar
-              className="-ml-3 h-9 w-9 select-none border border-white"
-              key={index}
-            >
-              <AvatarImage src={participant.image_url} alt={participant.name} />
-              <AvatarFallback className="bg-gray-500">
-                {children}
-              </AvatarFallback>
-            </Avatar>
-          )
-        })}
-        {hasMoreParticipants && (
-          <Avatar className="-ml-3 h-9 w-9 border border-white">
-            <AvatarFallback
-              className={`bg-indigo-600 ${
-                users?.length - 3 > 999 ? 'text-[0.625rem]' : 'text-[0.6875rem]'
-              } font-medium text-white`}
-            >
-              {`+${users?.length - 3}`}
-            </AvatarFallback>
+    <div className="ml-3 flex">
+      {users?.slice(0, 3).map((participant, index) => {
+        return (
+          <Avatar
+            className="-ml-3 h-9 w-9 select-none border-2 border-slate-850"
+            key={index}
+          >
+            <AvatarImage src={participant.image_url} alt={participant.name} />
+            <AvatarFallback className="bg-gray-500">{children}</AvatarFallback>
           </Avatar>
-        )}
-      </div>
+        )
+      })}
+      {hasMoreParticipants && (
+        <Avatar className="-ml-3 h-9 w-9 border-2 border-slate-850">
+          <AvatarFallback
+            className={`bg-indigo-600 ${
+              users?.length - 3 > 999 ? 'text-[0.625rem]' : 'text-[0.6875rem]'
+            } font-medium text-white`}
+          >
+            {`+${users?.length - 3}`}
+          </AvatarFallback>
+        </Avatar>
+      )}
     </div>
   )
 }
