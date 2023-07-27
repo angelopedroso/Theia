e809fbcbe6bbfc8c4d06c48b6e5b03181fe19017
angelopedroso <solonasusonly0@gmail.com>
2023-07-26 23:31:49 -0300

    feat:updated header component to allow outer class

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,18 +1,26 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 export interface HeaderProps {
   title: string
   subtitle?: string
+  className?: React.HtmlHTMLAttributes<HTMLHeadingElement>['className']
 }
 
-export function Header(props: HeaderProps) {
+export function Header({
+  title = '',
+  subtitle = '',
+  className = '',
+}: HeaderProps) {
   return (
-    <header className="mb-14 w-full">
-      <h2 className="text-sm font-bold leading-normal text-zinc-50 2xs:text-lg sm:text-2xl">
-        {props.title}
+    <header className={cn('mb-14 w-full', className)}>
+      <h2 className="font-menu text-xl font-bold leading-normal text-zinc-50 2xs:text-2xl">
+        {title}
       </h2>
-      <span className="text-2xs font-medium leading-normal text-indigo-600 2xs:text-xs sm:text-sm">
-        {props.subtitle}
+      <span
+        className={`text-2xs font-medium leading-normal text-indigo-600 2xs:text-xs sm:text-sm ${className}`}
+      >
+        {subtitle}
       </span>
     </header>
   )
