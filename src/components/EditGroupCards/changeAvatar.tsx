a79974b49c39912791a200c6fe96cb6c25a89bdc
angelopedroso <solonasusonly0@gmail.com>
2023-07-26 23:32:44 -0300

    feat: created change avatar image component

---
+++ src/components/EditGroupCards/changeAvatar.tsx
@@ -0,0 +1,72 @@
+import React, { ChangeEvent, useRef, useState } from 'react'
+import { DefaultGroupIcon } from '../groupIcon'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  Input,
+  Label,
+} from '@/components/ui'
+
+export interface ChangeAvatarProps {
+  imageUrl?: string
+  imageUrlFormReturn: (url: string) => void
+}
+
+export function ChangeAvatar({
+  imageUrl = '',
+  imageUrlFormReturn,
+}: ChangeAvatarProps) {
+  const [avatar, setAvatar] = useState<string>('')
+  const inputImageRef = useRef<HTMLInputElement>(null)
+
+  function handleAvatarChange(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files?.length) {
+      const file = URL.createObjectURL(e.target.files.item(0) as Blob)
+
+      if (file) {
+        setAvatar(file)
+
+        const reader = new FileReader()
+
+        reader.readAsDataURL(e.target.files.item(0) as Blob)
+
+        reader.onloadend = () => {
+          imageUrlFormReturn(reader.result as string)
+        }
+      }
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-start space-y-1 xs:items-center md:flex-row md:space-y-0">
+      <div className="w-auto md:w-52">
+        <Label className="text-base leading-6 text-slate-300">Avatar</Label>
+      </div>
+
+      <div className="flex flex-1 items-center gap-5">
+        <Avatar className="h-12 w-12">
+          <AvatarImage src={avatar || imageUrl} />
+          <AvatarFallback className="bg-gray-500">
+            <DefaultGroupIcon />
+          </AvatarFallback>
+        </Avatar>
+        <Input
+          type="file"
+          accept="image/jpeg, image/png"
+          className="hidden"
+          ref={inputImageRef}
+          onChangeCapture={handleAvatarChange}
+        />
+        <Button
+          variant={'outline'}
+          onClick={() => inputImageRef.current?.click()}
+          className="border-slate-700 bg-transparent text-sm font-medium text-indigo-600 shadow-md hover:bg-indigo-600 hover:text-white"
+        >
+          Change
+        </Button>
+      </div>
+    </div>
+  )
+}
