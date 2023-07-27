1d13ce2b37ddb19c9a6690d597c52b95071af3a1
angelopedroso <solonasusonly0@gmail.com>
2023-07-26 23:32:24 -0300

    feat: created group edit page

---
+++ src/app/groups/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getDBData } from '@/api/getDBData'
+import { EditGroupSettings } from '@/components/EditGroupCards/editGroupSettings'
+import { Header } from '@/components/ui/header'
+import React from 'react'
+
+export interface GroupPageIdProps {
+  searchParams: { id: string }
+}
+
+export default async function GroupPageId({ searchParams }: GroupPageIdProps) {
+  const groupId = searchParams.id
+
+  const data: GroupsProps = await getDBData({
+    uri: `groups/${groupId}`,
+  })
+
+  return (
+    <div className="flex h-full flex-col p-8">
+      <div className="mb-6 border-b border-slate-600 pb-6">
+        <Header title={data.group_info.name} className="mb-0" />
+      </div>
+      <main className="flex justify-end">
+        <EditGroupSettings data={data} />
+      </main>
+    </div>
+  )
+}
