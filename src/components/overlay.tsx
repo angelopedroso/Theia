e469ea65b98e80488ee01c8dba50b819d13fa294
angelopedroso <solonasusonly0@gmail.com>
2023-07-29 20:15:06 -0300

    fix: added flexbox into overlay

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -3,14 +3,14 @@ import React, { ReactNode } from 'react'
 
 export interface OverlayProps {
   children: ReactNode
-  close: () => void
+  toggle: () => void
 }
 
-export function Overlay({ close, children }: OverlayProps) {
+export function Overlay({ toggle, children }: OverlayProps) {
   return (
     <motion.div
-      className="fixed inset-0 z-50 bg-slate-950/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 flex items-center justify-center"
-      onClick={close}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-slate-950/80 p-4 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      onClick={toggle}
       variants={{
         open: { backgroundColor: 'rgba(0,0,0,0.9)' },
         closed: { backgroundColor: 'rgba(0,0,0,0)' },
