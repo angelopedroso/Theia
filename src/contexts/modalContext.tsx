f31752181bf16b06219893d5aaea513b58866973
angelopedroso <solonasusonly0@gmail.com>
2023-07-31 14:50:53 -0300

    feat: added alert context function

--- src/contexts/modalContext.tsx
+++ src/contexts/modalContext.tsx
@@ -4,8 +4,10 @@ import { ReactNode, createContext, useState } from 'react'
 
 interface ModalContextProps {
   toggle: boolean
+  alert: boolean
   name: string
   toggleModal: (name: string) => void
+  handleAlertModal: (value: boolean) => void
 }
 
 interface ModalProviderProps {
@@ -19,14 +21,21 @@ export const modalContext = createContext<ModalContextProps>(
 export function ModalProvider({ children }: ModalProviderProps) {
   const [toggle, setToggle] = useState(false)
   const [name, setName] = useState('')
+  const [alert, setAlert] = useState(false)
 
   function toggleModal(name: string) {
     setToggle((state) => !state)
     setName(name)
   }
 
+  function handleAlertModal(value: boolean) {
+    setAlert(value)
+  }
+
   return (
-    <modalContext.Provider value={{ toggle, toggleModal, name }}>
+    <modalContext.Provider
+      value={{ toggle, toggleModal, name, alert, handleAlertModal }}
+    >
       {children}
     </modalContext.Provider>
   )
