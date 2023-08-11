7c2e4545182d845adfe8cd4a798b0edcf723b2d9
angelopedroso <solonasusonly0@gmail.com>
2023-08-11 14:05:15 -0300

    feat: created updateBot

--- src/api/actions/query.ts
+++ src/api/actions/query.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { updateFormProps } from '@/components/GroupPage/GroupForm/groupSettingsForm'
+import { BotFormularyProps } from '@/components/SideMenu/components/botFormulary'
 import { api } from '@/lib/axios'
 
 export type GroupRequestData = Omit<updateFormProps, 'group_name'> & {
@@ -26,4 +27,8 @@ export async function removeParticipant(data: RemoveParticipantProps) {
     method: 'get',
     data,
   })
+}
+
+export async function updateBot(data: BotFormularyProps) {
+  await api.put('/bot', data)
 }
